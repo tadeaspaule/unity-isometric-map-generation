63f542b5b052ee27e19af525ea68f26877358530
Tadeas Paule <tpaule.personal@gmail.com>
2019-10-09 07:22:07 +0100

    basic symbol assigning functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public List<Friendly> party;
+
+    public SymbolHolder symbolHolder;
+
+    List<string> symbols = new List<string>(new string[]{"donut","square"});
+    string selectedSymbol = null;
+    Dictionary<string,int> assignedSymbols = new Dictionary<string, int>();
+
+    public List<EnemyObject> enemies;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        symbolHolder.SetupSymbols(symbols);
+        foreach (string s in symbols) {
+            assignedSymbols.Add(s,-1);
+        }
     }
 
     // Update is called once per frame
@@ -25,14 +36,49 @@ public class GameManager : MonoBehaviour
 
     }
 
-    public void PartyCharacterClicked(int index)
+    public void SymbolClicked(GameObject symbol)
     {
+        int index = int.Parse(symbol.name);
+        selectedSymbol = symbols[index];
+        symbolHolder.HighlightSymbol(index);
+    }
+
+    public void PartyCharacterClicked(GameObject partyCharacter)
+    {
+        int index = int.Parse(partyCharacter.name);
 
     }
 
-    public void EnemyCharacterClicked(Enemy enemy)
+    public void EnemyCharacterClicked(GameObject enemy)
     {
+        Debug.Log($"Clicking on enemy character with name {enemy.name}");
+        int index = int.Parse(enemy.name.Substring(6));
+        symbolHolder.ClearHighlights();
         
+        if (selectedSymbol != null) {
+            // put symbol on enemy
+            if (assignedSymbols[selectedSymbol] >= 0) {
+                // reassigning symbol to a different enemy
+                enemies[assignedSymbols[selectedSymbol]].ClearSymbol();
+            }
+            // check if that enemy had a different symbol already
+            foreach (string key in assignedSymbols.Keys) {
+                if (assignedSymbols[key] == index) {
+                    // enemy has a symbol
+                    if (assignedSymbols[key].Equals(selectedSymbol)) return; // assigning the same symbol
+                    else {
+                        enemies[index].ClearSymbol(); // clearing a different symbol
+                        assignedSymbols[key] = -1;
+                        break;
+                    }
+                }
+            }
+            enemies[index].SetSymbol(selectedSymbol);
+            assignedSymbols[selectedSymbol] = index;
+        }
+        else {
+            // clicked enemy without a symbol, maybe some other interaction?
+        }
     }
 
     #endregion
---
+++ Assets/Scripts/TilemapManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapManager : MonoBehaviour
+{
+    public Tilemap colliderMap;     // this has collider stuff and it's used everywhere tiles are inaccessible
+    public Tilemap frontwallsMap;   // the front bottom edges of all rooms, to give it a quasi 3D look
+
+    public class TilemapLevel
+    {
+        public Tilemap baseMap;     // holds the bottom-most tilemap of that level, so mostly floor tiles
+        public Tilemap detailMap;   // holds details tiles that sit directly on top of base tiles - decoration
+        public Tilemap wallsMap;    // holds the top edge walls of that level
+        public Tilemap doorsMap;    // if applicable, holds the doors to the next level. Otherwise null
+    }
+    public List<TilemapLevel> levels;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
