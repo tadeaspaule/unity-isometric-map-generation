0f2b20758cac515f63014540e309628f4f11991d
Tadeas Paule <tpaule.personal@gmail.com>
2019-10-09 09:55:46 +0100

    add colliders

--- Assets/Scripts/TilemapManager.cs
+++ Assets/Scripts/TilemapManager.cs
@@ -15,6 +15,14 @@ public class TilemapManager : MonoBehaviour
         public Tilemap detailMap;   // holds details tiles that sit directly on top of base tiles - decoration
         public Tilemap wallsMap;    // holds the top edge walls of that level
         public Tilemap doorsMap;    // if applicable, holds the doors to the next level. Otherwise null
+    
+        public void ClearAll()
+        {
+            baseMap.ClearAllTiles();
+            if (detailMap != null) detailMap.ClearAllTiles();
+            if (wallsMap != null) wallsMap.ClearAllTiles();
+            if (doorsMap != null) doorsMap.ClearAllTiles();
+        }
     }
     public List<TilemapLevel> levels;
 
@@ -25,20 +33,24 @@ public class TilemapManager : MonoBehaviour
     void Start()
     {
         frontwallsMap.ClearAllTiles();
-        colliderMap.ClearAllTiles();
+        // colliderMap.ClearAllTiles(); DONT DO THIS IT somehow turns off the collider component or smth
     }
 
     public void SetupMap(int[,] blocks, int blockSize, int blockGap, int mapSize)
     {
-        Tilemap baseMap = levels[0].baseMap;
-        baseMap.ClearAllTiles();
+        levels[0].ClearAll();
+        for (int y = -1; y < (blockSize+blockGap)*mapSize+1; y++) {
+            for (int x = -1; x < (blockSize+blockGap)*mapSize+1; x++) {
+                colliderMap.SetTile(new Vector3Int(x,y,0),groundTile);
+            }
+        }
         for (int y = 0; y < mapSize; y++) {
             for (int x = 0; x < mapSize; x++) {
                 int startX = (blockSize+blockGap)*x;
                 int startY = (blockSize+blockGap)*y;
                 for (int cellY = startY; cellY < startY + blockSize; cellY++) {
                     for (int cellX = startX; cellX < startX + blockSize; cellX++) {
-                        baseMap.SetTile(new Vector3Int(cellX,cellY,0),groundTile);
+                        FillGroundTile(new Vector3Int(cellX,cellY,0));
                     }
                 }
                 // connect block above
@@ -47,7 +59,7 @@ public class TilemapManager : MonoBehaviour
                         // same room -> fill gap
                         for (int cellY = startY+blockSize; cellY < startY+blockSize+blockGap; cellY++) {
                             for (int cellX = startX; cellX < startX + blockSize; cellX++) {
-                                baseMap.SetTile(new Vector3Int(cellX,cellY,0),groundTile);
+                                FillGroundTile(new Vector3Int(cellX,cellY,0));
                             }
                         }
                     }
@@ -56,7 +68,7 @@ public class TilemapManager : MonoBehaviour
                         int bridgeX = startX + Random.Range(0,blockSize-2);
                         for (int cellY = startY+blockSize; cellY < startY+blockSize+blockGap; cellY++) {
                             for (int cellX = bridgeX; cellX < bridgeX + 2; cellX++) {
-                                baseMap.SetTile(new Vector3Int(cellX,cellY,0),groundTile);
+                                FillGroundTile(new Vector3Int(cellX,cellY,0));
                             }
                         }
                     }
@@ -67,7 +79,7 @@ public class TilemapManager : MonoBehaviour
                         // same room -> fill gap
                         for (int cellY = startY; cellY < startY+blockSize; cellY++) {
                             for (int cellX = startX+blockSize; cellX < startX + blockSize+blockGap; cellX++) {
-                                baseMap.SetTile(new Vector3Int(cellX,cellY,0),groundTile);
+                                FillGroundTile(new Vector3Int(cellX,cellY,0));
                             }
                         }
                     }
@@ -76,7 +88,7 @@ public class TilemapManager : MonoBehaviour
                         int bridgeY = startY + Random.Range(0,blockSize-2);
                         for (int cellY = bridgeY; cellY < bridgeY+2; cellY++) {
                             for (int cellX = startX+blockSize; cellX < startX+blockSize+blockGap; cellX++) {
-                                baseMap.SetTile(new Vector3Int(cellX,cellY,0),groundTile);
+                                FillGroundTile(new Vector3Int(cellX,cellY,0));
                             }
                         }
                     }
@@ -86,12 +98,18 @@ public class TilemapManager : MonoBehaviour
                     if (blocks[y,x+1] == blocks[y+1,x] && blocks[y,x+1] == blocks[y,x]) {
                         for (int cellY = startY+blockSize; cellY < startY+blockSize+blockGap; cellY++) {
                             for (int cellX = startX+blockSize; cellX < startX+blockSize+blockGap; cellX++) {
-                                baseMap.SetTile(new Vector3Int(cellX,cellY,0),groundTile);
+                                FillGroundTile(new Vector3Int(cellX,cellY,0));
                             }
                         }
                     }
                 }
             }
         }
+    }
+
+    void FillGroundTile(Vector3Int pos)
+    {
+        levels[0].baseMap.SetTile(pos,groundTile);
+        colliderMap.SetTile(pos,null);
     }
 }
